35b9f96fa6406f2eb3e022c47e2cb18b8bf9f27d
Psyquatas <psyquatas@Psyquataz.local>
2026-06-19 21:13:14 +0200

    ajout matomo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,23 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           strategy="afterInteractive"
         />
 
+        <Script id="matomo-tag" strategy="afterInteractive">
+          {`
+            var _paq = window._paq = window._paq || [];
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="https://mizucat.matomo.cloud/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true;
+              g.src='https://cdn.matomo.cloud/mizucat.matomo.cloud/matomo.js';
+              s.parentNode.insertBefore(g,s);
+            })();
+          `}
+        </Script>
+
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
